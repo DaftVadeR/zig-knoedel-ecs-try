9342d91b52c678e02a8f5c54c3d7850004991369
Ross Davidson <tsuyoizuse@gmail.com>
2026-03-10 10:15:56 +0200

    Enable drawing of enemies.

--- src/enemy.zig
+++ src/enemy.zig
@@ -6,11 +6,15 @@ const game = @import("game.zig");
 // const enemy = @import("components/enemy.zig");
 
 const enemy_spawner = @import("enemy_spawner.zig");
+const enemy_movement = @import("enemy_movement.zig");
+const enemy_shape = @import("enemy_shape.zig");
 
 const kn = game.kn;
 
 pub fn plugin(app: *kn.App) !void {
     try app.addPlugin(enemy_spawner);
+    // try app.addPlugin(enemy_movement);
+    try app.addPlugin(enemy_shape);
 
     try app.addSystemEx(game.Schedule.update, &update, kn.InState(game.AppState.gameplay));
     try app.addSystemEx(game.Schedule.draw, &draw, kn.InState(game.AppState.gameplay));
